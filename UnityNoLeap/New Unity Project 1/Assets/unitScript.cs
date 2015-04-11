80dfd6eef5dfe31eff2fa7f6e917134e36a775db
Pat <patrickdevarney@gmail.com>
2015-04-11 03:46:56 -0500

    Pat first AI stuff again

---
+++ UnityNoLeap/New Unity Project 1/Assets/unitScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unitScript : MonoBehaviour {
+
+	public GameObject target;
+
+	public int health;
+	public float speed;
+	public int team;
+	public int attackDmg;
+	public float attackSpeed;
+	public float threshold = 1.0f;
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (target.transform.position);
+		if (target.transform.position.x - gameObject.transform.position.x < threshold) {
+			Destroy(this.gameObject);
+		}
+	}
+
+
+}
