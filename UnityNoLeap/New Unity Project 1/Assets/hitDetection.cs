c5ed05e36ee8508cd16d4185c7e9384aee83fb95
Pat <patrickdevarney@gmail.com>
2015-04-11 18:25:36 -0500

    hope this works

---
+++ UnityNoLeap/New Unity Project 1/Assets/hitDetection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitDetection : MonoBehaviour {
+
+	unitScript parentUnit;
+
+	void Start(){
+		parentUnit = transform.parent.gameObject.GetComponent<unitScript> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.transform.tag.Equals("Projectile")){
+			Debug.Log("projectile hit something");
+			if(other.GetComponent<bulletScript>().team != parentUnit.team){
+				parentUnit.takeDamage(other.GetComponent<bulletScript>().damage);
+				Destroy(other.gameObject);
+			}
+		}
+	}
+}
